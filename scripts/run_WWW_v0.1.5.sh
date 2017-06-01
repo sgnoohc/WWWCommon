f4c6d46a0cfa27ecf8e52c14547bb5a49d482f25
Philip Chang <sgnoohc@gmail.com>
2017-06-01 14:12:26 -0700

    not perfomring hadd in one step

--- scripts/run_WWW_v0.1.5.sh
+++ scripts/run_WWW_v0.1.5.sh
@@ -10,4 +10,5 @@ TAG=$1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 EXTRAOPTION=$2
 
-ditto_runana -d /nfs-7/userdata/bhashemi/WWW_babies/WWW_v0.1.5/skim/ -l ${DIR}/../inputlist/WWW_v0.1.5_inputlist.txt -t WWW_v0.1.5_${TAG} -f ${EXTRAOPTION} && hadd_WWW_v0.1.5.sh WWW_v0.1.5_${TAG}
+ditto_runana -d /nfs-7/userdata/bhashemi/WWW_babies/WWW_v0.1.5/skim/ -l ${DIR}/../inputlist/WWW_v0.1.5_inputlist.txt -t WWW_v0.1.5_${TAG} -f ${EXTRAOPTION}
+#	&& hadd_WWW_v0.1.5.sh WWW_v0.1.5_${TAG}
