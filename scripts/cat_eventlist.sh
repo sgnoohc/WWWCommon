b46c8c731ba6246764a1c94a57fa4a4e7282c5d2
Philip Chang <sgnoohc@gmail.com>
2017-06-01 15:24:01 -0700

    update

--- scripts/cat_eventlist.sh
+++ scripts/cat_eventlist.sh
@@ -1,2 +1,3 @@
-cat WWW_v0.1.5_baselinev2/log/www_2l* | grep SSmmHJ > eventlist_HJ.txt
-cat WWW_v0.1.5_baselinev2/log/www_in* | grep SSmm: > eventlist_bobak.txt
+ls WWW_v0.1.5_baselinev2/log/$1*
+cat WWW_v0.1.5_baselinev2/log/$1* | grep SSmmHJ > eventlist_HJ.txt
+#cat WWW_v0.1.5_baselinev2/log/www_in* | grep SSmm: > eventlist_bobak.txt
--- scripts/sync_hadd_WWW_v0.1.5.sh
+++ scripts/sync_hadd_WWW_v0.1.5.sh
@@ -11,7 +11,7 @@ ditto_hadd \
   -3 'wjets_' '-#' 'W' \
   -o 'dy_' -O 'Z' \
   -r 'wz_1l,wz_3l,wz_ln' -R 'WZ' \
-  -b 'ww_2l2nu,ww_lnuqq' -B 'WW' \
+  -b 'ww_2l2nu,ww_lnuqq,wpwp' -B 'WW' \
   -4 'zz_2l,zz_2q,zz_4l' '-$' 'ZZ' \
   -g 'ttg_,tth_,ttw_incl,ttz_incl' -G 'ttX' \
   -k 'tzq_,stt_,sttw_,sttwll_' -K 'tX' \
