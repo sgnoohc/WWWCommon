3d5aac8e95f5b56c0c6ef694c1355e08ba95bcce
Philip Chang <sgnoohc@gmail.com>
2017-06-02 00:28:33 -0700

    print cutflow

--- scripts/print_cutflow.sh
+++ scripts/print_cutflow.sh
@@ -2,7 +2,7 @@ if [ -z $1 ]; then
   echo "Must provide the output directory where the cutflow histogram exists"
   exit
 fi
-ditto_plot -v SR_counts  -i $1/haddoutput/ -e '--print_cutflow --print_cutflow_bins 1,2,3,4,5,6' -d plots_$1/
+ditto_plot -v SR_counts  -i $1/haddoutput/ -e '--print_cutflow --print_cutflow_bins 1,2,3,4,5,6' -d cutflows_$1/
 cat plots_$1/SMWWWAnalysis_SR_counts_counter.csv
 echo ""
 cat plots_$1/SMWWWAnalysis_SR_counts_rawcounter.csv
